c363f4fe356ca36a3225d838d93049f4574254f6
Jimmy <jajalaba@yahoo.com.tw>
2020-09-30 14:10:42 +0800

    關掉sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,7 +34,7 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    `gatsby-plugin-sitemap`,
+
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
