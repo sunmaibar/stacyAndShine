91692786eb97b47b82f06ba5d0944eec65b09a84
Jimmy <jajalaba@yahoo.com.tw>
2021-04-16 10:32:13 +0800

    try226

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,24 @@
-import React from "react"
-import { graphql } from "gatsby"
-import { Layout, Hero, About, Projects, Survey, Slider, GridProjects } from "../components"
-import SEO from "../components/seo"
+import React from 'react'
+import { graphql } from 'gatsby'
+import {
+  Layout,
+  Hero,
+  About,
+  Projects,
+  Survey,
+  Slider,
+  GridProjects,
+} from '../components'
+import SEO from '../components/seo'
 const HomePage = ({ data }) => {
   const {
-    allAirtable: { nodes: projects }
+    allAirtable: { nodes: projects },
   } = data
   return (
     <Layout>
       <SEO
-        title="從心生活坊"
-        description="新北市三重區空間出租，適合辦生日party
+        title='從心生活坊'
+        description='新北市三重區空間出租，適合辦生日party
 租借場地慶生，抓週各種活動
 瑜伽（寶寶，親子，大人），精油芳療
 希塔開課
@@ -20,11 +28,11 @@ const HomePage = ({ data }) => {
 命理紫微
 親子座談會
 品酒會
-水晶解讀，脈輪開通"
+水晶解讀，脈輪開通'
       />
       <Hero projects={projects} />
       <About />
-      <Projects title="最新活動" projects={projects} />
+      <Projects title='最新活動' projects={projects} />
       <Survey />
       <Slider />
     </Layout>
@@ -33,7 +41,11 @@ const HomePage = ({ data }) => {
 
 export const query = graphql`
   {
-    allAirtable(filter: { table: { eq: "Projects" } }, sort: { fields: data___date, order: DESC }, limit: 3) {
+    allAirtable(
+      filter: { table: { eq: "Projects" } }
+      sort: { fields: data___date, order: DESC }
+      limit: 3
+    ) {
       nodes {
         id
         data {
